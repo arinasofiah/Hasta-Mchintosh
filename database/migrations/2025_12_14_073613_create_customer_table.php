f185fdb82e8038cc46ae3bf66e53bc9802e75421
Khairunnisa <khairunnisanajani2@gmail.com>
2025-12-14 15:53:56 +0800

    Update 2025_12_14_073613_create_customer_table.php

--- database/migrations/2025_12_14_073613_create_customer_table.php
+++ database/migrations/2025_12_14_073613_create_customer_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('customer', function (Blueprint $table) {
             $table->id(matricNumber);
 
-            $table->string('matricNumber')->unique();
             $table->string('name');
             $table->string('licenseNumber');
             $table->string('icNumber');
