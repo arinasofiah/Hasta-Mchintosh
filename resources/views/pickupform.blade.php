63e1b4c286ee0666e5ee95ad483ae99c2fcc06c7
ManteStr <mante.str101@gmail.com>
2025-12-16 12:02:37 +0800

    header changes

---
+++ resources/views/pickupform.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hasta Travel & Tour</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- Bootstrap --}}
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- Bootstrap --}}
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    {{-- Custom CSS --}}
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+@include('profile.partials.header')
+<div id="body">
+    {{-- Page content goes here --}}
+</div>
+
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,47 +27,7 @@
 </head>
 
 <body>
-
-<div id="header">
-    <img id="logo" src="{{ asset('img/hasta_logo.jpg') }}">
-
-    <div id="menu">
-        <button class="head_button">Home</button>
-        <button class="head_button">Vehicles</button>
-        <button class="head_button">Details</button>
-        <button class="head_button">About Us</button>
-        <button class="head_button">Contact Us</button>
-    </div>
-
-    <div id="profile">
-        <div id="profile-container">
-            <img id="pfp" src="{{ asset('img/racc_icon.png') }}">
-
-            <div id="profile-dropdown">
-                @guest
-                    <a href="{{ route('login') }}" class="dropdown-item">Login</a>
-                    <a href="{{ route('register') }}" class="dropdown-item">Register</a>
-                @endguest
-
-                @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="dropdown-item">Logout</button>
-                    </form>
-                @endauth
-            </div>
-        </div>
-
-        @guest
-            <a id="username" href="{{ route('login') }}">Log in</a>
-        @endguest
-
-        @auth
-            <span id="username">{{ Auth::user()->name }}</span>
-        @endauth
-    </div>
-</div>
-
+@include('profile.partials.header')
 <div id="body">
     {{-- Page content goes here --}}
 </div>
