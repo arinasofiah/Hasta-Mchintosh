fe30decba598e4be765e9c6b3d046f4858f08a4d
ManteStr <mante.str101@gmail.com>
2025-12-19 18:27:53 +0800

    update pickup page ui

--- resources/views/pickupform.blade.php
+++ resources/views/pickupform.blade.php
@@ -14,14 +14,38 @@
 
     {{-- Custom CSS --}}
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pickup.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
 @include('profile.partials.header')
 <div id="body">
-    {{-- Page content goes here --}}
+    
+<div class="container">
+    <div class="pickup-layout">
+        <aside class="vehicle-card">
+            <p id="res_txt">Reserved Vehicle</p>
+            <p id="car_name">Perodua Axia 2024<p>
+            <p id="car_type">Hatchback</p>
+
+            <img src="{{ asset('img/redcar.png') }}">
+
+            <p id="car_det">Full Vehicle Details<p>
+            <ul class="features">
+                <li> <img src="{{ asset('img/car_icon.png') }}"><span>Automatic</span></li>
+                <li><img src="{{ asset('img/car_icon.png') }}"><span>RON 95</span></li>
+                <li><img src="{{ asset('img/car_icon.png') }}"><span>Air Conditioner</span></li>
+                <li> <img src="{{ asset('img/car_icon.png') }}"><span>5 Seats</span></li>
+                <li><img src="{{ asset('img/car_icon.png') }}"><span>4 Doors</span></li>
+                <li><img src="{{ asset('img/car_icon.png') }}"><span>1.3k km</span></li>
+            </ul>
+            <p id="day_pr">RM130 / Day</p>
+            <p id="all_pr">Total MYR 530</p>
+        </aside>
+    </div>
+</div>
+
 </div>
 
 
