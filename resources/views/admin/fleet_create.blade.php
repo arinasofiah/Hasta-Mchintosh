9ff62f83966d10b1f90ee0531835ffa5b6c269b3
JunLer <junlerlee1130@gmail.com>
2026-01-05 07:34:15 +0800

    update admin

--- resources/views/admin/fleet_create.blade.php
+++ resources/views/admin/fleet_create.blade.php
@@ -44,8 +44,7 @@
         <a href="{{ route('admin.fleet') }}" class="nav-item active"> Fleet</a> 
         <a href= "{{ route('admin.customers') }}" class="nav-item">Customer</a>
         <a href="{{ route('admin.staff') }}" class="nav-item">Staff</a>
-        <a href="{{ route('admin.promotions') }}" class="nav-item"> Promotions</a>
-        <a href="#" class="nav-item"> Settings</a>
+        <a href="{{ route('admin.promotions') }}" class="nav-item">Promotions</a>
     
     </div>
 
