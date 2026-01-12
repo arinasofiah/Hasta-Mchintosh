9e7e12e0faac46f2b82504566ba6619b8d00741c
Khairunnisa <khairunnisanajani2@gmail.com>
2026-01-12 13:21:26 +0800

    Update bookingform.blade.php

--- resources/views/bookingform.blade.php
+++ resources/views/bookingform.blade.php
@@ -4,21 +4,10 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<<<<<<< Updated upstream
 <title>HASTA - Booking Details</title>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <link href="{{ asset('css/header.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-=======
-<title>HASTA – Booking Details</title>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link href="{{ asset('css/header.css') }}" rel="stylesheet">
-<link href="{{ asset('css/footer.css') }}" rel="stylesheet">
-<link href="{{ asset('css/vehicles.css') }}" rel="stylesheet">
->>>>>>> Stashed changes
 
 <style>
 /* ====== Reset & Body ====== */
